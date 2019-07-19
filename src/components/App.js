da6969c6853138360989e9e5c3f93429488fe09f
daschne8 <daschne8@gmail.com>
2019-07-19 11:25:18 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,38 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event.target.value
+      }
+    })
+    console.log(event.target.value)
+  }
+
+  onFindPetsClick = () => {
+    console.log("clicked")
+    let url = `/api/pets`
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`
+    }
+    fetch(url)
+    .then(res => res.json())
+    .then((result) => {
+      this.setState({
+        pets: result
+      })
+    })
+  }
+
+  onAdoptPet = (petId) => {
+    const pets = this.state.pets.map( pet => {
+      return pet.id === petId ? {...pet, isAdopted: true} : pet
+    })
+    this.setState({pets})
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +56,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser  pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,15 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+
+  generatePetCards = () =>{
+    return this.props.pets.map( pet => <Pet onAdoptPet={this.props.onAdoptPet} key={pet.id} pet={pet}/>)
+  }
+
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards">
+            {this.generatePetCards()}
+          </div>
   }
 }
 
